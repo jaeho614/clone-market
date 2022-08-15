e0ed7ca1ef7820ddda80011b625fb3dae5afe4e7
jaeho614 <jaeho614a@gmail.com>
2022-08-15 23:11:38 +0900

    enter

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,5 +9,5 @@ module.exports = {
   },
   darkMode: "class", //설정이 media로 되어있으면 darkmode 설정은 환경설정을 따라간다. class로 되어있으면 darkmode는 활성화 되지 않음,
   // class인 상태에서 dark모드를 쓰려면 부모선택자에 dark를 넣어주면 된다.
-  plugins: [],
+  plugins: [require("@tailwindcss/forms")], //require("@tailwindcss/forms") 부분은 npm i @tailwindcss/forms를 입력하여 설치후 여기 적어주면 된다.
 };
