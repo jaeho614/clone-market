6141f9f431b8b6d28526f7192ecc154829fdeadd
jaeho614 <jaeho614a@gmail.com>
2022-08-28 23:43:11 +0900

    useForm

---
+++ pages/forms.tsx
@@ -0,0 +1,34 @@
+import { useForm } from "react-hook-form";
+
+export default function Forms() {
+  const { register, handleSubmit } = useForm();
+  const onValid = () => {
+    console.log("i an valid");
+  };
+  return (
+    <form onSubmit={handleSubmit(onValid)}>
+      <input
+        {...register("username", {
+          required: true,
+        })}
+        type="text"
+        placeholder="Username"
+      />
+      <input
+        {...register("email", {
+          required: true,
+        })}
+        type="email"
+        placeholder="Email"
+      />
+      <input
+        {...register("password", {
+          required: true,
+        })}
+        type="password"
+        placeholder="Password"
+      />
+      <input type="submit" value="Create Account" />
+    </form>
+  );
+}
