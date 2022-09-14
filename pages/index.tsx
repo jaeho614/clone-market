acbdbbb027516accbf03917347ff7c7961630647
jaeho614 <jaeho614a@gmail.com>
2022-09-15 01:00:39 +0900

    products favorite Products

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import Layout from "@components/layout";
 import useUser from "@libs/client/useUser";
 import Head from "next/head";
 import useSWR from "swr";
+import { Product } from "@prisma/client";
 
 interface ProductsResponse {
   ok: boolean;
@@ -14,7 +15,6 @@ interface ProductsResponse {
 const Home: NextPage = () => {
   const { user, isLoading } = useUser();
   const { data } = useSWR<ProductsResponse>("/api/products");
-  console.log(data);
   return (
     <Layout title="홈" hasTabBar>
       <Head>
