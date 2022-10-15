1fc3338df50dd8d2502f2b6cb6e137af183148cf
jaeho614 <jaeho614a@gmail.com>
2022-10-16 00:20:14 +0900

    Mutations and Refresh

--- pages/streams/[id].tsx
+++ pages/streams/[id].tsx
@@ -36,7 +36,10 @@ const Stream: NextPage = () => {
   const router = useRouter();
   const { register, handleSubmit, reset } = useForm<MessageForm>();
   const { data, mutate } = useSWR<StreamReponse>(
-    router.query.id ? `/api/streams/${router.query.id}` : null
+    router.query.id ? `/api/streams/${router.query.id}` : null,
+    {
+      refreshInterval: 1000, //configuration object : SWR이 서버에서 얼마나 새로고침 되기를 원하는지를 명시할 수 있게 해준다.
+    }
   );
   const [sendMessage, { loading, data: sendMessageData }] = useMutation(
     `/api/streams/${router.query.id}/messages`
@@ -44,13 +47,24 @@ const Stream: NextPage = () => {
   const onValid = (form: MessageForm) => {
     if (loading) return;
     reset();
+    mutate(
+      prev =>
+        prev &&
+        ({
+          ...prev,
+          stream: {
+            ...prev.stream,
+            messages: [
+              ...prev.stream.messages,
+              { id: Date.now(), message: form.message, user: { ...user } },
+            ],
+          },
+        } as any),
+      false
+    );
     sendMessage(form);
   };
-  useEffect(() => {
-    if (sendMessageData && sendMessageData.ok) {
-      mutate();
-    }
-  }, [sendMessageData, mutate]);
+
   return (
     <Layout canGoBack>
       <div className="py-10 px-4  space-y-4">
