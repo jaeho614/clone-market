ee686c63cea23101aab5d597b682ddb43745c90e
jaeho614 <jaeho614a@gmail.com>
2022-10-16 01:39:42 +0900

    pagination

--- libs/server/client.ts
+++ libs/server/client.ts
@@ -4,7 +4,7 @@ declare global {
   var client: PrismaClient | undefined;
 }
 
-const client = new PrismaClient();
+const client = new PrismaClient(); // {log: ["query"]} 를 ()에 넣어주면 query의 크기를 확인할 수 있다.
 
 if (process.env.NODE_ENV === "development") global.client = client;
 
